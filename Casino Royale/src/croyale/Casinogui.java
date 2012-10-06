b189cbd50f9946c5a1ccbfb00dc6b35014a47f63
Chris <Chris@192.168.2.5>
2012-10-06 02:24:04 -0400

    Minor change to test git push.

--- Casino Royale/src/croyale/Casinogui.java
+++ Casino Royale/src/croyale/Casinogui.java
@@ -2,7 +2,7 @@ package croyale;
 
 public class Casinogui {
 	public static void main(String[] args) {
-		System.out.println("I am a Casino!");
+		System.out.println("I am a Casino! Yay!");
 	}
 
 }
