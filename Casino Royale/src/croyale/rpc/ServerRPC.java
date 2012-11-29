b3bde0a6b2ec3449f3e8df030bda47c708095076
chuckmcm <cmcmullen@supplyone.com>
2012-11-29 15:34:16 -0500

    rmi changes

--- Casino Royale/src/croyale/rpc/ServerRPC.java
+++ Casino Royale/src/croyale/rpc/ServerRPC.java
@@ -3,20 +3,24 @@ package croyale.rpc;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 
 import croyale.Database;
 
 public class ServerRPC implements Constants
 {
 	// Bind a remote object to be used for RPC with clients
-	public static void init(Database db) throws RemoteException, MalformedURLException, UnknownHostException
+	public static void init(Database db, Registry registry) throws RemoteException, MalformedURLException, UnknownHostException, AlreadyBoundException
 	{
 		String name = InetAddress.getLocalHost().getHostName();
 		String addr = InetAddress.getLocalHost().getHostAddress();
 		
-		System.out.println(name + " " + addr);
-		Naming.rebind("//" + addr + ":2020/" + name, new ServerHost(db));
+		//System.out.println("//" + addr + ":2020/" + name);
+		
+		//Naming.rebind("//" + addr + ":2020/" + SERVER_NAME, new ServerHost(db));
+		registry.rebind(SERVER_NAME, new ServerHost(db));
 	}
 }
