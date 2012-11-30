7a306095413adf0832ce02b44511c973807f1cfb
Chris <chardulak@gmail.com>
2012-11-30 02:19:46 -0500

    Minor graphics change

--- Casino Royale/src/croyale/games/BlackjackView.java
+++ Casino Royale/src/croyale/games/BlackjackView.java
@@ -52,67 +52,67 @@ public class BlackjackView{
 		// Initialize containers
 		contentPane.removeAll();
 		contentPane.setBackground(new Color(0,176,80));
-		JLayeredPane mainPane = new JLayeredPane();
+		JLabel mainPane = new ImagePanel(new ImageIcon("src/croyale/resources/blackjack.jpg").getImage());
 		
 		// Set background image as panel
 		//JLabel backgroundPane = new ImagePanel(new ImageIcon("src/croyale/resources/BlackjackTable.png").getImage());
-		backgroundPane = new ImagePanel(new ImageIcon("src/croyale/resources/blackjack.jpg").getImage());
-		backgroundPane.setOpaque(false);
+		//backgroundPane = new ImagePanel(new ImageIcon("src/croyale/resources/blackjack.jpg").getImage());
+		//backgroundPane.setOpaque(false);
 
 		//Layout the components.
-		JPanel formContainer = new JPanel();
-		formContainer.setLayout(new BoxLayout(formContainer,BoxLayout.Y_AXIS));
-		formContainer.setOpaque(false);
+		//JPanel formContainer = new JPanel();
+		mainPane.setLayout(new BoxLayout(mainPane,BoxLayout.Y_AXIS));
+		mainPane.setOpaque(false);
 		//formContainer.setBounds(300, 200, 400, 260);
 		//JButton m_hitBtn = new JButton(new ImageIcon("src/croyale/resources/HIT poker chip.png"));
 		//JButton standButton= new JButton(new ImageIcon("src/croyale/resources/HIT poker chip.png"));
 		//m_hitBtn.setPreferredSize(new Dimension(20,20));
-		
+		/*
 		m_enterBetL.setLocation(10, 100);
 		m_enterBetL.setLayout(new BoxLayout(m_enterBetL, BoxLayout.X_AXIS));
 		//m_enterBetL.setPreferredSize(new Dimension(100,5));
 		JLabel m_enterBetLText = new JLabel("Enter amount of Bet:");
 		m_enterBetL.add(m_enterBetLText);
 		m_enterBetL.setMaximumSize(m_enterBetL.getPreferredSize());
-		formContainer.add(this.m_enterBetL);
-		formContainer.add(Box.createRigidArea(new Dimension(5,0)));
+		mainPane.add(this.m_enterBetL);
+		mainPane.add(Box.createRigidArea(new Dimension(5,0)));
 		m_betTf.setMaximumSize(m_betTf.getPreferredSize());
-		formContainer.add(this.m_betTf);
-		formContainer.add(Box.createRigidArea(new Dimension(5,0)));
+		mainPane.add(this.m_betTf);
+		mainPane.add(Box.createRigidArea(new Dimension(5,0)));
 
 		
-		formContainer.add(this.m_betBtn);
+		mainPane.add(this.m_betBtn);
 		m_enterPlayL.setLayout(new BoxLayout(m_enterPlayL, BoxLayout.X_AXIS));
 		m_enterPlayL.setPreferredSize(new Dimension(100,20));
 		JLabel m_enterPlayLText = new JLabel("Enter play (Hit or Stand)");
 		m_enterPlayL.add(m_enterPlayLText);
-		formContainer.add(this.m_enterPlayL);
-		formContainer.add(Box.createRigidArea(new Dimension(5,0)));
+		mainPane.add(this.m_enterPlayL);
+		mainPane.add(Box.createRigidArea(new Dimension(5,0)));
 		m_currentPlayTf.setMaximumSize(m_currentPlayTf.getPreferredSize());
-		formContainer.add(this.m_currentPlayTf);
-		formContainer.add(this.m_playBtn);
+		mainPane.add(this.m_currentPlayTf);
+		mainPane.add(this.m_playBtn);
 		
 		m_balanceL.setLocation(10, 100);
 		m_balanceL.setLayout(new BoxLayout(m_balanceL, BoxLayout.Y_AXIS));
 		m_balanceL.setPreferredSize(new Dimension(100,20));
 		JLabel m_balanceLText = new JLabel("balance:");
 		m_balanceL.add(m_balanceLText);
-		formContainer.add(this.m_balanceL);
-		formContainer.add(Box.createRigidArea(new Dimension(5,0)));
+		mainPane.add(this.m_balanceL);
+		mainPane.add(Box.createRigidArea(new Dimension(5,0)));
 		m_balanceTf.setLayout(new BoxLayout(m_balanceTf, BoxLayout.X_AXIS));
 		m_balanceTf.setMaximumSize(m_balanceTf.getPreferredSize());
-		formContainer.add(this.m_balanceTf);
+		mainPane.add(this.m_balanceTf);
 		
-		formContainer.add(this.m_outputTa);
+		mainPane.add(this.m_outputTa);
 		
-		
-		mainPane.setPreferredSize(new Dimension(800,1000));
+		*/
+	//	mainPane.setPreferredSize(new Dimension(800,1000));
 		//mainPane.add(formContainer,1);
-		mainPane.add(backgroundPane,2);
+		//mainPane.add(backgroundPane,2);
 		contentPane.add(mainPane);
-		backgroundPane.setBounds(50,0,600,800);
-		formContainer.setBounds(50,100,600,800);
-		contentPane.setBounds(0,0,600,800);
+		//backgroundPane.setBounds(50,0,600,800);
+		//mainPane.setBounds(50,100,600,800);
+		//contentPane.setBounds(0,0,600,800);
 		contentPane.revalidate();
 	}
 	
